2154ce3383d5cf3ba2c645a7959a8dc3263f97c7
Taki24 <61392818+Taki24@users.noreply.github.com>
2020-09-16 21:09:26 +0200

    Updates

---
+++ precache-manifest.9c6d9476a802b8ae74374c36caedaf08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55c66e1d2b5c39ca2e7de63383c1547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e64c5abb6c983d09fc",
+    "url": "/static/css/main.280eb8ff.chunk.css"
+  },
+  {
+    "revision": "ba09441e97cb4007d4d2",
+    "url": "/static/js/2.ef0ad048.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/static/js/2.ef0ad048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e64c5abb6c983d09fc",
+    "url": "/static/js/main.180b6f48.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
