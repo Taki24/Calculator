3cdf3827589d27cedbbed6c59d1fde8688397fdf
Taki24 <61392818+Taki24@users.noreply.github.com>
2020-09-17 10:49:40 +0200

    Updates

---
+++ precache-manifest.32f9d996151146119555e27bc32eabec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdb2a6d7051fc9dd6b48545f30967bd",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "23ceeb4eb927d60ccea1",
+    "url": "/Calculator/static/css/main.35670f3d.chunk.css"
+  },
+  {
+    "revision": "ba09441e97cb4007d4d2",
+    "url": "/Calculator/static/js/2.ef0ad048.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Calculator/static/js/2.ef0ad048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ceeb4eb927d60ccea1",
+    "url": "/Calculator/static/js/main.71956b8f.chunk.js"
+  },
+  {
+    "revision": "2a6aa57c400e22e8c935",
+    "url": "/Calculator/static/js/runtime-main.0dd3f83c.js"
+  }
+]);
